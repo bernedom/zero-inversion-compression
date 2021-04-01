f048f5003e0ed56063883d6be49945d8f8dde61c
Dominik Berner <dominik.berner@gmail.com>
2021-04-01 10:21:56 +0200

    Read file into vector for further processing

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # zero-inversion-compression
 
-A non-lossless file compression algorithm with compression rate up to 100% of the original file size. 
+Zero-Inversion-Compression (zic) non-lossless file compression algorithm with compression rate up to 100% of the original file size. 
+
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,26 @@
-fn main() {}
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+fn read_into_vec(filename: &String) -> Vec<u8> {
+    let mut f = File::open(&filename).expect(&format!("File {} not found", filename));
+    let metadata = std::fs::metadata(&filename)
+        .expect(&format!("Unable to read meta data from file {}", filename));
+    let mut buf = vec![0; metadata.len() as usize];
+    f.read(&mut buf)
+        .expect("Could not read file contents into buffer");
+
+    buf
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: zero-inversion-compression <filename>");
+        return;
+    }
+
+    let input_file = &args[1];
+    let mut data  = read_into_vec(input_file);
+}
